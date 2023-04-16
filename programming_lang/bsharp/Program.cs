87c6fcc70efd7c3a9c49f7966f314b1e067bb3fd
randomusert <127417963+randomusert@users.noreply.github.com>
2023-04-16 18:35:28 +0300

    added litle shell

--- programming_lang/bsharp/Program.cs
+++ programming_lang/bsharp/Program.cs
@@ -12,8 +12,45 @@ namespace bsharp
         {
             while (true)
             {
-                Console.WriteLine("ok");
+                Console.Write("bsharp(beta)>");
+                var line = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(line)) 
+                {
+                    return;
+                }
+                if (line == "1 + 2 * 3") 
+                {
+                    Console.WriteLine("9"); 
+                }
+                else 
+                {
+                    Console.WriteLine("ERROR!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                }
             }
         }
+    }
+    class SyntaxToken
+    {
+        public SyntaxToken(SyntaxKind kind,int position, string text)
+        {
+            
+        }
+    }
+
+    enum SyntaxKind
+    {
+    }
+
+    class Lexer
+    {
+        private readonly string _text;
+        private int _position;
+        public Lexer(string _text)
+        {
+            Text = _text;
+        }
+
+        public SyntaxToken NextToken() { }
+        public string Text { get; }
     }
 }
